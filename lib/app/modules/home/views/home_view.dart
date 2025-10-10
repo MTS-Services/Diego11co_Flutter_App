22afedd919502678ff0cadf52f04e706652c931d
Md. Al Arafat <arafathossain593@gmail.com>
2025-10-10 17:29:42 +0600

    update

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -15,69 +15,71 @@ class HomeView extends GetView<HomeController> {
     return Scaffold(
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(AssetPath.appLogoPng),
-            SizedBox(height: 8.h),
-            ProfileProgressCard(
-              name: "Diego",
-              message: "Ciao!",
-              progress: 0.4,
-              imageUrl: "https://i.pravatar.cc/150?img=3",
-              onNotificationTap: () {
-                print("Notification tapped!");
-              },
-            ),
-            SizedBox(height: 10.h),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Home",
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(AssetPath.appLogoPng),
+              SizedBox(height: 8.h),
+              ProfileProgressCard(
+                name: "Diego",
+                message: "Ciao!",
+                progress: 0.4,
+                imageUrl: "https://i.pravatar.cc/150?img=3",
+                onNotificationTap: () {
+                  print("Notification tapped!");
+                },
+              ),
+              SizedBox(height: 10.h),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Home",
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
-            ),
-            GridView.builder(
-              padding: EdgeInsets.zero,
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 5.w,
-                mainAxisSpacing: 5.h,
-                childAspectRatio: 0.70,
-              ),
-              itemCount: 4,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: (){
-                    Get.toNamed('/video-player');
-                  },
-                  child: CourseProgressCard(
-                    imageUrl:
-                    'https://images.pexels.com/photos/3852143/pexels-photo-3852143.jpeg',
-                    status: 'In Corso',
-                    title: 'Datore di lavoro (Nuovo)',
-                    hours: '16 ore',
-                    progress: 0.6,
-                  ),
-                );
-              },
-            ),
-            SizedBox(height: 25.h),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  Get.offNamed('/course-view');
+              GridView.builder(
+                padding: EdgeInsets.zero,
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 5.w,
+                  mainAxisSpacing: 5.h,
+                  childAspectRatio: 0.70,
+                ),
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: (){
+                      Get.toNamed('/video-player');
+                    },
+                    child: CourseProgressCard(
+                      imageUrl:
+                      'https://images.pexels.com/photos/3852143/pexels-photo-3852143.jpeg',
+                      status: 'In Corso',
+                      title: 'Datore di lavoro (Nuovo)',
+                      hours: '16 ore',
+                      progress: 0.6,
+                    ),
+                  );
                 },
-                child: Text('Riprendi la formazione'),
               ),
-            ),
-          ],
+              SizedBox(height: 25.h),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Get.offNamed('/course-view');
+                  },
+                  child: Text('Riprendi la formazione'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
